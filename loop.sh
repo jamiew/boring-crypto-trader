9646913b59f99518afbc32483c19a17f801a0ed3
Jamie Wilkinson <jamie@jamiedubs.com>
2017-12-04 21:57:16 -0500

    Buy mountains of coins

--- loop.sh
+++ loop.sh
@@ -2,11 +2,11 @@
 
 # set to every 3 hours
 # 0.01 ETH @ $250... 8x day ~= $25/day
-# 0.0002 BTC @ $11k... 8x day ~= $16/day
+# 0.0003 BTC @ $11k... 8x day ~= $24/day
 time=$((3 * 60 * 60))
 
 while [ 1 ]; do
-  # bundle exec ruby trader.rb buy 0.0002 BTC
+  bundle exec ruby trader.rb buy 0.0003 BTC
   bundle exec ruby trader.rb buy 0.01 ETH
   sleep $time
 done
