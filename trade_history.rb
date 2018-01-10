24d332921abfd0abb9890d78baf599ce2b30e1c3
Jamie Wilkinson <jamie@jamiedubs.com>
2018-01-09 22:13:48 -0500

    Fixing more things

--- trade_history.rb
+++ trade_history.rb
@@ -14,27 +14,43 @@ def client
 end
 
 
-def aggregate_stats(date, coin, fills)
-  data = {
-    prices: fills.map{|f| f[:price] },
-    price: fills.map{|f| f[:price] }.inject(:+),
-    sizes: fills.map{|f| f[:size] },
-    size: fills.map{|f| f[:size] }.inject(:+),
-    values: fills.map{|f| f[:value] },
-    value: fills.map{|f| f[:value] }.inject(:+),
-  }
 
-  highest_price = data[:prices].sort[0]
-  lowest_price = data[:prices].sort[-1]
-  avg_price = (data[:price]/fills.length).to_f
 
-  fields = [date, data[:size], coin, fills.length, data[:value], avg_price.to_f, highest_price, lowest_price]
-  puts fields.join(', ')
+def print_aggregate_stats(fill_groups, print_summary_stats=false)
+  results = []
+  fill_groups.each do |group,fills|
+    date, coin = group.split(' ')
 
-  # $stderr.puts "Bought #{data[:size]} #{coin} in #{fills.length} transactions total=$#{data[:value]}"
-  # $stderr.puts "Avg price: #{avg_price.to_f}"
-  # $stderr.puts "Highest price: #{highest_price}"
-  # $stderr.puts "Lowest price: #{lowest_price}"
+    data = {
+      date: date,
+      coin: coin,
+      prices: fills.map{|f| f[:price] },
+      price: fills.map{|f| f[:price] }.inject(:+),
+      sizes: fills.map{|f| f[:size] },
+      size: fills.map{|f| f[:size] }.inject(:+),
+      values: fills.map{|f| f[:value] },
+      value: fills.map{|f| f[:value] }.inject(:+),
+    }
+
+    highest_price = data[:prices].sort[0]
+    lowest_price = data[:prices].sort[-1]
+    avg_price = (data[:price]/fills.length).to_f
+
+    fields = [date, data[:size], coin, fills.length, data[:value], avg_price.to_f, highest_price, lowest_price]
+    puts fields.join(', ')
+
+    if print_summary_stats
+      $stderr.puts "coin=#{coin} date=#{date}"
+      $stderr.puts "Bought #{data[:size]} #{coin} in #{fills.length} transactions total=$#{data[:value]}"
+      $stderr.puts "Avg price: #{avg_price.to_f}"
+      $stderr.puts "Highest price: #{highest_price}"
+      $stderr.puts "Lowest price: #{lowest_price}"
+    end
+
+    results << data
+  end
+
+  results
 end
 
 def print_fill(fill)
@@ -69,41 +85,39 @@ end
 
 #### WORK ###
 
-# Breaks rate limit...
-# pp client.trade_history
-
-# start_time = Date.parse('2017-12-01')
-# end_time = Date.parse('2017-12-28')
-# granularities {60, 300, 900, 3600, 21600, 86400}
-# pp client.price_history(granularity: 86400, start: start_time, end: end_time)
-# exit
-
+action = ARGV[0].to_s
+$stderr.puts "action=#{action.inspect}"
 
 fills = client.fills.map{|f| parse_fill(f) }
-$stderr.puts "Found #{fills.length} trades"
+$stderr.puts "Found #{fills.length} trades ..."
 
-$stderr.puts "\n----- every trade -----"
-fills.sort_by{|f| f[:created_at] }.each do |fill|
-  unless fill[:side] == 'buy' || fill[:side] == 'sell'
-    raise "Don't know how to deal with fill.side=#{fill[:side].inspect} yet"
+if action == 'daily'
+
+  $stderr.puts "\n----- daily stats -----"
+  fill_groups = fills.group_by{|f| f[:created_at].strftime('%Y-%m-%d') + ' ' + f[:coin] }.sort_by{|k,v| k }
+  print_aggregate_stats(fill_groups)
+
+elsif action == 'monthly'
+
+  $stderr.puts "\n----- monthly stats -----"
+  fill_groups = fills.group_by{|f| f[:created_at].strftime('%Y-%m') + ' ' + f[:coin] }.sort_by{|k,v| k }
+  print_aggregate_stats(fill_groups, true)
+
+elsif action == 'price_history'
+
+  raise 'Not Yet Implemented'
+  # start_time = Date.parse('2017-12-01')
+  # end_time = Date.parse('2017-12-28')
+  # granularities {60, 300, 900, 3600, 21600, 86400}
+  # pp client.price_history(granularity: 86400, start: start_time, end: end_time)
+  # exit
+
+else
+
+  $stderr.puts "\n----- every trade -----"
+  fills.sort_by{|f| f[:created_at] }.each do |fill|
+    print_fill(fill)
   end
 
-  print_fill(fill)
 end
-
-
-$stderr.puts "\n----- daily stats -----"
-fill_groups = fills.group_by{|f| f[:created_at].strftime('%Y-%m-%d') + ' ' + f[:coin] }.sort_by{|k,v| k }
-fill_groups.each do |group,fills|
-  date, coin = group.split(' ')
-  aggregate_stats(date, coin, fills)
-end
-
-$stderr.puts "\n----- monthly stats -----"
-fill_groups = fills.group_by{|f| f[:created_at].strftime('%Y-%m') + ' ' + f[:coin] }.sort_by{|k,v| k }
-fill_groups.each do |group,fills|
-  date, coin = group.split(' ')
-  aggregate_stats(date, coin, fills)
-end
-
 
